aee341cd8e122ce7e0825ce4d2c1a168b1b3eff9
ezequieliribarren <ezequielgiribarren@gmail.com>
2023-05-21 09:52:46 -0300

    RedPsicoHispana

---
+++ js/sweetAlert.js
@@ -0,0 +1,77 @@
+function emigrar () {
+    
+let emigrar1 = document.querySelector ('.emigrar1')
+let emigrar2 = document.querySelector ('.emigrar2')
+let emigrar3 = document.querySelector ('.emigrar3')
+let emigrar4 = document.querySelector ('.emigrar4')
+let emigrar5 = document.querySelector ('.emigrar5')
+let emigrar6 = document.querySelector ('.emigrar6')
+
+
+emigrar1.onclick = () => {
+    Swal.fire({
+        html: '<div class="sweet"> <h3 class="titleSweet">¿Qué es el sindrome de ulices?</h3> <p class= "pSweet">Conocido también como síndrome de estrés crónico y múltiple.Tiene que ver con un fuerte malestar que viven algunas personas que migran, por los cambios y el pasaje de un lugar que les es familiar a un espacio extranjero. <br><br> El síndrome de Ulises va más allá de un duelo migratorio, dado que al duelo esperado y normal, se le suma la vivencia subjetiva de la persona que se siente sobrepasada por la situación y sin recursos para afrontarla. <br><br> Si sentís que los cambios en tu nueva vida son más de lo que podés procesar, no dudes en buscar ayuda psicológica. Muchas veces hablar y ordenar la cabeza es todo lo que se necesita. </p> </div>',
+width: '80%',
+        backdrop:"true",
+        confirmButtonText: 'Volver',
+        confirmButtonColor: '#498563',
+      })
+}
+emigrar1.onclick = () => {
+    Swal.fire({
+        html: '<div class="sweet"> <h3 class="titleSweet">¿Qué es el sindrome de ulices?</h3> <p class= "pSweet">Conocido también como síndrome de estrés crónico y múltiple.Tiene que ver con un fuerte malestar que viven algunas personas que migran, por los cambios y el pasaje de un lugar que les es familiar a un espacio extranjero. <br><br> El síndrome de Ulises va más allá de un duelo migratorio, dado que al duelo esperado y normal, se le suma la vivencia subjetiva de la persona que se siente sobrepasada por la situación y sin recursos para afrontarla. <br><br> Si sentís que los cambios en tu nueva vida son más de lo que podés procesar, no dudes en buscar ayuda psicológica. Muchas veces hablar y ordenar la cabeza es todo lo que se necesita. </p> </div>',
+width: '80%',
+        backdrop:"true",
+        confirmButtonText: 'Volver',
+        confirmButtonColor: '#498563',
+      })
+    }
+    emigrar2.onclick = () => {
+        Swal.fire({
+            html: '<div class="sweet"> <h3 class="titleSweet">¿Qué es el sindrome de ulices?</h3> <p class= "pSweet">Conocido también como síndrome de estrés crónico y múltiple.Tiene que ver con un fuerte malestar que viven algunas personas que migran, por los cambios y el pasaje de un lugar que les es familiar a un espacio extranjero. <br><br> El síndrome de Ulises va más allá de un duelo migratorio, dado que al duelo esperado y normal, se le suma la vivencia subjetiva de la persona que se siente sobrepasada por la situación y sin recursos para afrontarla. <br><br> Si sentís que los cambios en tu nueva vida son más de lo que podés procesar, no dudes en buscar ayuda psicológica. Muchas veces hablar y ordenar la cabeza es todo lo que se necesita. </p> </div>',
+    width: '80%',
+            backdrop:"true",
+            confirmButtonText: 'Volver',
+            confirmButtonColor: '#498563',
+          })
+        }
+        emigrar3.onclick = () => {
+            Swal.fire({
+                html: '<div class="sweet"> <h3 class="titleSweet">¿Qué es el sindrome de ulices?</h3> <p class= "pSweet">Conocido también como síndrome de estrés crónico y múltiple.Tiene que ver con un fuerte malestar que viven algunas personas que migran, por los cambios y el pasaje de un lugar que les es familiar a un espacio extranjero. <br><br> El síndrome de Ulises va más allá de un duelo migratorio, dado que al duelo esperado y normal, se le suma la vivencia subjetiva de la persona que se siente sobrepasada por la situación y sin recursos para afrontarla. <br><br> Si sentís que los cambios en tu nueva vida son más de lo que podés procesar, no dudes en buscar ayuda psicológica. Muchas veces hablar y ordenar la cabeza es todo lo que se necesita. </p> </div>',
+        width: '80%',
+                backdrop:"true",
+                confirmButtonText: 'Volver',
+                confirmButtonColor: '#498563',
+              })
+            }
+            emigrar4.onclick = () => {
+                Swal.fire({
+                    html: '<div class="sweet"> <h3 class="titleSweet">¿Qué es el sindrome de ulices?</h3> <p class= "pSweet">Conocido también como síndrome de estrés crónico y múltiple.Tiene que ver con un fuerte malestar que viven algunas personas que migran, por los cambios y el pasaje de un lugar que les es familiar a un espacio extranjero. <br><br> El síndrome de Ulises va más allá de un duelo migratorio, dado que al duelo esperado y normal, se le suma la vivencia subjetiva de la persona que se siente sobrepasada por la situación y sin recursos para afrontarla. <br><br> Si sentís que los cambios en tu nueva vida son más de lo que podés procesar, no dudes en buscar ayuda psicológica. Muchas veces hablar y ordenar la cabeza es todo lo que se necesita. </p> </div>',
+            width: '80%',
+                    backdrop:"true",
+                    confirmButtonText: 'Volver',
+                    confirmButtonColor: '#498563',
+                  })
+                }
+                emigrar5.onclick = () => {
+                    Swal.fire({
+                        html: '<div class="sweet"> <h3 class="titleSweet">¿Qué es el sindrome de ulices?</h3> <p class= "pSweet">Conocido también como síndrome de estrés crónico y múltiple.Tiene que ver con un fuerte malestar que viven algunas personas que migran, por los cambios y el pasaje de un lugar que les es familiar a un espacio extranjero. <br><br> El síndrome de Ulises va más allá de un duelo migratorio, dado que al duelo esperado y normal, se le suma la vivencia subjetiva de la persona que se siente sobrepasada por la situación y sin recursos para afrontarla. <br><br> Si sentís que los cambios en tu nueva vida son más de lo que podés procesar, no dudes en buscar ayuda psicológica. Muchas veces hablar y ordenar la cabeza es todo lo que se necesita. </p> </div>',
+                width: '80%',
+                        backdrop:"true",
+                        confirmButtonText: 'Volver',
+                        confirmButtonColor: '#498563',
+                      })
+                    }
+                    emigrar6.onclick = () => {
+                        Swal.fire({
+                            html: '<div class="sweet"> <h3 class="titleSweet">¿Qué es el sindrome de ulices?</h3> <p class= "pSweet">Conocido también como síndrome de estrés crónico y múltiple.Tiene que ver con un fuerte malestar que viven algunas personas que migran, por los cambios y el pasaje de un lugar que les es familiar a un espacio extranjero. <br><br> El síndrome de Ulises va más allá de un duelo migratorio, dado que al duelo esperado y normal, se le suma la vivencia subjetiva de la persona que se siente sobrepasada por la situación y sin recursos para afrontarla. <br><br> Si sentís que los cambios en tu nueva vida son más de lo que podés procesar, no dudes en buscar ayuda psicológica. Muchas veces hablar y ordenar la cabeza es todo lo que se necesita. </p> </div>',
+                    width: '80%',
+                            backdrop:"true",
+                            confirmButtonText: 'Volver',
+                            confirmButtonColor: '#498563',
+                          })
+                        }
+
+}
+
+emigrar ();
